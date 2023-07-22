677a52793c5211b86068cc4adaf17ff6ac8abfd3
Aleksandr Shibanov <shadow.sanya28@gmail.com>
2023-07-22 09:00:23 +0500

    add foo2 and add docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,13 @@
 def foo(text):
+    """большая буква"""
     return text.upper()
+
+def foo2(text):
+    """каждое новое слово с большой буквы"""
+    return text.title()
 
 user_input = input("Введите текс")
 
+
 print(foo(user_input))
+print(foo2(user_input))
